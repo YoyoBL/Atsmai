c9ef08099ae5993b2658d4e6fad4ee5bd2ad3c09
YoyoBLemail <yoelbl2@gmail.com>
2024-04-05 14:05:21 +0300

    welcome content

--- src/app/[lang]/(public-access)/welcome/page.jsx
+++ src/app/[lang]/(public-access)/welcome/page.jsx
@@ -1,5 +1,40 @@
+import RegisterBtn from "@/components/navs/registerBtn";
+import SignInBtn from "@/components/navs/signInBtn";
+
 const Welcome = () => {
-   return <div>Welcome</div>;
+   return (
+      <section className="grid justify-items-center w-full">
+         <div className="shadow-lg md:w-3/4 grid md:grid-cols-12 place-items-center gap-1 p-5">
+            <div className="col-span-7">
+               <h1 className="text-3xl text-center">Welcome to Atsmai</h1>
+               <div className="p-3 md:w-3/4"></div>
+               <p>
+                  Welcome to our income and expense management application!{" "}
+                  <br />
+                  We're thrilled to have you here and excited to help you take
+                  control of your finances. <br /> <br />
+                  If you're tired of having no idea what's coming in or out, and
+                  you're looking to make order in your business, you're in the
+                  right place!
+               </p>
+            </div>
+
+            <div className="divider divider-horizontal col-auto hidden md:flex"></div>
+
+            <div className="grid place-items-center h-full col-span-4 md:text-center">
+               <div className="divider w-full md:hidden"></div>
+               <p>Want to see what Atsmai looks like?</p>
+               <button className="btn btn-primary">Click here</button>
+               <div className="divider w-full md:hidden"></div>
+
+               <div className="grid grid-cols-2 gap-3 mt-auto">
+                  <SignInBtn />
+                  <RegisterBtn />
+               </div>
+            </div>
+         </div>
+      </section>
+   );
 };
 
 export default Welcome;
