6c3893c92f7aeda17e859a790ed488f4ed3345ef
YoyoBLemail <yoelbl2@gmail.com>
2024-04-07 17:02:03 +0300

    welcome

--- src/app/[lang]/(public-access)/welcome/page.jsx
+++ src/app/[lang]/(public-access)/welcome/page.jsx
@@ -3,13 +3,13 @@ import SignInBtn from "@/components/navs/signInBtn";
 
 const Welcome = () => {
    return (
-      <section className="grid justify-items-center w-full">
-         <div className="shadow-lg md:w-3/4 grid md:grid-cols-12 place-items-center gap-1 p-5">
+      <section className="grid justify-items-center w-full h-full">
+         <div className="my-auto shadow-lg h-fit md:w-3/4 grid md:grid-cols-12 place-items-center gap-1 p-5">
             <div className="col-span-7">
                <h1 className="text-3xl text-center">Welcome to Atsmai</h1>
                <div className="p-3 md:w-3/4"></div>
                <p>
-                  Welcome to our income and expense management application!{" "}
+                  Welcome to our income and expense management application!
                   <br />
                   We're thrilled to have you here and excited to help you take
                   control of your finances. <br /> <br />
@@ -21,16 +21,16 @@ const Welcome = () => {
 
             <div className="divider divider-horizontal col-auto hidden md:flex"></div>
 
-            <div className="grid place-items-center h-full col-span-4 md:text-center">
-               <div className="divider w-full md:hidden"></div>
-               <p>Want to see what Atsmai looks like?</p>
-               <button className="btn btn-primary">Click here</button>
+            <div className="grid place-items-center gap-3 h-full col-span-12 md:col-span-4 md:text-center">
                <div className="divider w-full md:hidden"></div>
 
                <div className="grid grid-cols-2 gap-3 mt-auto">
                   <SignInBtn />
                   <RegisterBtn />
                </div>
+               <div className="divider w-full md:hidden"></div>
+               <p>Want to see what Atsmai looks like?</p>
+               <button className="btn btn-primary">Click here</button>
             </div>
          </div>
       </section>
