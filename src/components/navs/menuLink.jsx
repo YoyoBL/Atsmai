8695e7dd2fd0df83e2cf05935d54f7ddfdc263e9
YoyoBLemail <yoelbl2@gmail.com>
2024-04-08 17:49:51 +0300

    close sidebar on click mobile

--- src/components/navs/menuLink.jsx
+++ src/components/navs/menuLink.jsx
@@ -9,9 +9,11 @@ const MenuLink = ({ link }) => {
    const { lang } = useParams();
    const currentPath = path.split("/")[2] || "";
    const isCurrentPath = currentPath === link.href;
+
    return (
       <li>
          <Link
+            onClick={closeSidebar}
             href={`/${lang}/${link.href}`}
             className={cn(
                "flex gap-3 p-3 hover:bg-base-300 rounded-xl",
@@ -26,3 +28,9 @@ const MenuLink = ({ link }) => {
 };
 
 export default MenuLink;
+
+function closeSidebar() {
+   const checkbox = document.getElementById("my-drawer-2");
+   const checked = checkbox.checked;
+   checkbox.checked = !checked;
+}
