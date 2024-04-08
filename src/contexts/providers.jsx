b3becab764cdf5b6717788804cf2ea0cefd056a2
YoyoBLemail <yoelbl2@gmail.com>
2024-04-08 17:47:24 +0300

    react-hot-toaster

--- src/contexts/providers.jsx
+++ src/contexts/providers.jsx
@@ -1,7 +1,24 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
 
 export function Providers({ children, session }) {
-   return <SessionProvider session={session}>{children}</SessionProvider>;
+   return (
+      <SessionProvider session={session}>
+         {children}
+         <Toaster
+            position="top-center"
+            reverseOrder={false}
+            gutter={8}
+            containerClassName=""
+            containerStyle={{}}
+            toastOptions={{
+               // Define default options
+               className: "",
+               duration: 5000,
+            }}
+         />
+      </SessionProvider>
+   );
 }
