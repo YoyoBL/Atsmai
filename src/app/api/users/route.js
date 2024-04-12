0fc155ddc337f7b55eccda3f88553be35463fc4d
YoyoBLemail <yoelbl2@gmail.com>
2024-04-12 11:32:36 +0300

    create user route

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,5 +1,27 @@
-export async function GET() {
-   await new Promise((resolve) => setTimeout(resolve, 1500));
+import dbConnect from "@/lib/mongoDbConnect";
+import User from "@/models/user.model";
+import bcrypt from "bcrypt";
 
-   return Response.json({ message: "Hello" });
+export async function POST(req) {
+   try {
+      await dbConnect();
+      // check if user exists
+      const body = await req.json();
+
+      const email = body.email.toLowerCase();
+      const exists = await User.findOne({ email });
+
+      if (exists) throw new Error("Email already registered.");
+      const hashedPassword = await bcrypt.hash(body.password, 12);
+      const newUser = await User.create({
+         ...body,
+         email,
+         password: hashedPassword,
+      });
+      const { firstName, lastName, _id } = newUser;
+      return Response.json({ firstName, lastName, email, _id: _id.toString() });
+   } catch (error) {
+      console.log(error);
+      return Response.json({ error: error.message }, { status: 400 });
+   }
 }
