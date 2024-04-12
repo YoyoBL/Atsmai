169e2d8ffa31ad32be1e2a0df70ee8899b36ec38
YoyoBLemail <yoelbl2@gmail.com>
2024-04-12 14:30:22 +0300

    adjustments

--- src/app/[lang]/(public-access)/about/page.jsx
+++ src/app/[lang]/(public-access)/about/page.jsx
@@ -3,16 +3,18 @@ import SignInBtn from "@/components/navs/signInBtn";
 import { getDictionary } from "@/lib/dictionary";
 
 const About = async ({ params: { lang } }) => {
-   const {
-      about: { headers, content },
-   } = await getDictionary(lang);
+   const { about } = await getDictionary(lang);
+   const { headers, content } = about;
+
    const featuresList = content.whatCanAtsmaiDo.featuresList;
 
    return (
-      <section className="grid justify-items-center w-full overflow-auto">
-         <div className="card bg-base-200">
-            <div className="card-body">
-               <h1 className="card-title text-3xl justify-center">About</h1>
+      <section className="grid justify-items-center w-full p-3 overflow-auto">
+         <div className="card bg-base-200 h-fit">
+            <div className="card-body ">
+               <h1 className="card-title text-3xl justify-center">
+                  {about.title}
+               </h1>
                <ul className="space-y-3">
                   <li>
                      <h2 className="text-xl divider divider-start">
