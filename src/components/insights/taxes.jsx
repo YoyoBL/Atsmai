8d3aa1ddb9a4c2402cbfe4310fe1671c0fd8909d
YoyoBLemail <yoelbl2@gmail.com>
2024-05-12 11:42:48 +0300

    edited taxes.jsx - Insights

--- src/components/insights/taxes.jsx
+++ src/components/insights/taxes.jsx
@@ -2,14 +2,17 @@ import { fetchEntries } from "@/actions/entries.actions copy";
 import InsightWrapper from "./insightWrapper";
 import { fetchTaxesEntries } from "@/actions/entries.actions";
 import InsightServerError from "./insightServerError";
-import { MAIN_CURRENCY } from "@/constants";
+import { MAIN_CURRENCY, VAT_PERCENTAGE } from "@/constants";
 
 const Taxes = async () => {
    const title = "Taxes estimates";
    const res = await fetchTaxesEntries();
    if (!res.ok) return <InsightServerError title={title} />;
-   const vat = res.data.vat.amount.toLocaleString();
-   const months = res.data.vat.months;
+
+   const entries = res.data.entries;
+   const vat =
+      entries.reduce((acc, entry) => acc + entry.amount, 0) * VAT_PERCENTAGE;
+   const months = res.data.months;
    return (
       <InsightWrapper title={`Taxes estimates`}>
          <div className="stats w-full shadow">
