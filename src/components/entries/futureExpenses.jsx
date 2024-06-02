29832bb8fe82a5ae92fb97044b18325e85dc9171
YoyoBLemail <yoelbl2@gmail.com>
2024-06-02 20:36:40 +0300

    display total future expenses

--- src/components/entries/futureExpenses.jsx
+++ src/components/entries/futureExpenses.jsx
@@ -4,6 +4,7 @@ import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
 import FutureExpenseCard from "./futureExpenseCard";
 import { useParams } from "next/navigation";
+import { MAIN_CURRENCY } from "@/constants";
 
 const FutureExpenses = ({ recurring = [] }) => {
    const [flip, setFlip] = useState(false);
@@ -11,6 +12,10 @@ const FutureExpenses = ({ recurring = [] }) => {
    const title = lang === "en" ? "Future Expenses" : "הוצאות עתידיות";
 
    if (!recurring.length) return;
+   const totalFutureExpenses = recurring.reduce(
+      (acc, cur) => acc + cur.amount,
+      0
+   );
 
    function handleFlip() {
       setFlip((flip) => !flip);
@@ -19,7 +24,8 @@ const FutureExpenses = ({ recurring = [] }) => {
       <div onClick={handleFlip} className="collapse bg-base-200">
          <input type="checkbox" className="min-h-0" />
          <div className="collapse-title text-gray-400  flex justify-between min-h-0 p-2">
-            {title}
+            {title} - {totalFutureExpenses}
+            {MAIN_CURRENCY}
             <label className={cn("swap swap-rotate", flip && "swap-active")}>
                <ChevronDownIcon className="swap-off h-5 w-5 " />
                <ChevronUpIcon className="swap-on h-5 w-5 " />
